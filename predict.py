3ffbfa2e53ff8c9714e023d2bdf43a4604b5fe42
Yarashayev Nurgeldi <nyarashbayev97@gmail.com>
2024-01-27 11:43:00 +0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,7 +1,9 @@
+import argparse
 import tensorflow as tf
-import numpy as np 
+import numpy as np
 import cv2
 
+# Load the model
 model = tf.keras.models.load_model("./models/braintumorclassidication.h5")
 classNames = ['Glioma', 'Meningioma', 'Pituitary', 'no_tumor']
 
@@ -10,8 +12,24 @@ def load_image(path):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
 
-
 def predict(image):
-    image = image/255.0
+    image = image / 255.0  # Normalize pixel values
     pred = model.predict(image)
-    return classNames[np.argmax(pred)]
+    predicted_class = classNames[np.argmax(pred)]
+    return predicted_class
+
+# Create an argument parser
+parser = argparse.ArgumentParser(description="Brain Tumor Classification")
+parser.add_argument("--image_path", required=True, help="Path to the image file")
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Load the image
+image = load_image(args.image_path)
+
+# Make prediction
+predicted_class = predict(image)
+
+# Print the prediction
+print("Predicted class:", predicted_class)
