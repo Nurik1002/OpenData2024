c8b4769c74e34bb0828281f146db7632f48eb491
Yarashayev Nurgeldi <nyarashbayev97@gmail.com>
2024-01-27 12:25:45 +0500

    Add static and template files

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templatees'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
