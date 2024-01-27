4c411b2e652866fcc7660fec22c16f95169a7919
Yarashayev Nurgeldi <nyarashbayev97@gmail.com>
2024-01-27 12:01:56 +0500

    Update

--- webapp/myapp/views.py
+++ webapp/myapp/views.py
@@ -1,3 +1,5 @@
-from ...predict import load_image, predict
+#from ...predict import load_image, predict
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'index.html', {})
