ccf1098a16057608dd66f86bcaefc5d1c5be5f57
Yarashayev Nurgeldi <nyarashbayev97@gmail.com>
2024-01-28 14:32:16 +0500

    Update

--- webapp/myapp/views.py
+++ webapp/myapp/views.py
@@ -31,7 +31,7 @@ def result(request, pk):
         img = load_image(os.path.join(settings.BASE_DIR, image_obj.image.path))
         pred, foiz = predict(img)
         context["predict"] = pred
-        context["foiz"] = foiz
+        context["foiz"] = round(foiz*100,2)
 
     except Image.DoesNotExist:
         raise Http404("Image does not exist")
